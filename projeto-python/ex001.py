2e67ce4f5c17acaf9d33615cf819f84fba8e6317
Flavio Luna <106634718+FlavioLuna765@users.noreply.github.com>
2022-12-11 14:38:16 -0300

    corrigindo projeto

---
+++ projeto-python/ex001.py
@@ -0,0 +1,12 @@
+nome = str(input('Digite seu nome? '))
+país = str(input('Digite seu país? '))
+cidade = str(input('Digite sua cidade?'))
+print('Seu nome é {}, seu país é {} , e sua cidade é {}'.format(nome, país, cidade,))
+
+
+
+
+
+
+
+
